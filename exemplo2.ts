3a2d2905bd87cd417a3634fe2274cb220ae0d677
Izabel Salvati <izabelsalvati@gmail.com>
2019-07-26 22:20:07 -0300

    Exemplo2 TS

---
+++ exemplo2.ts
@@ -0,0 +1,12 @@
+let quantidade: number = 5
+
+quantidade = "5"
+
+console.log("quantidade atual é"+ quantidade)
+
+quantidade = quantidade +1
+
+console.log("quantidade atualizada é" + quantidade)
+
+
+
